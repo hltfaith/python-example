9aebb6e647eef3b2f8f1e4609d5cc170bf941639
Changhao <wu_chang_hao@qq.com>
2018-03-13 12:36:46 +0800

    Create run.py

---
+++ course/bin/run.py
@@ -0,0 +1,9 @@
+import os,sys
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+sys.path.append(BASE_DIR)
+
+from core import main
+
+if __name__ == '__main__':
+    run = main.menu()
+    run.menu_system()
