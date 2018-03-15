b4a3e8de4d1e8e94f08196ec85492864f2a4ef68
Changhao <wu_chang_hao@qq.com>
2018-03-16 00:45:55 +0800

    201803160045

---
+++ luffy/module3/socket/sshremote_socket3/client.py
@@ -0,0 +1,16 @@
+import socket
+
+phone = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+phone.connect(('127.0.0.1', 9901))
+while True:
+    # 1. 发命令
+    cmd = input('>>: ').strip()
+    if not cmd: continue
+    phone.send(cmd.encode('utf-8'))
+
+    # 2. 拿命令的结果, 并打印
+    data = phone.recv(1024)
+    print(data.decode('utf-8'))
+
+phone.close()
