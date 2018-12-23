6be9532047821a1ed4ec0538c0e689d86dbc84dd
Changhao <wu_chang_hao@qq.com>
2018-12-23 09:39:05 +0800

    Create 面向对象私有属性.py

---
+++ frame/class/use/面向对象私有属性.py
@@ -0,0 +1,13 @@
+
+1. __dict__
+class Person:
+    静态变量 = 123
+ 
+print(Person.__dict__) #内置的双下划线方法
+
+执行输出：
+{'__doc__': None, '静态变量': 123, '__module__': '__main__', '__weakref__': <attribute '__weakref__' of 'Person' objects>, '__dict__': <attribute '__dict__' of 'Person' objects>}
+
+2.
+
+
