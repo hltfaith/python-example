04d7dbef58ccebd16d6f12a2c778ffc87dc858ed
Changhao <wu_chang_hao@qq.com>
2018-12-23 09:41:28 +0800

    Update 面向对象私有属性.py

--- frame/class/use/面向对象私有属性.py
+++ frame/class/use/面向对象私有属性.py
@@ -8,6 +8,13 @@ print(Person.__dict__) #内置的双下划线方法
 执行输出：
 {'__doc__': None, '静态变量': 123, '__module__': '__main__', '__weakref__': <attribute '__weakref__' of 'Person' objects>, '__dict__': <attribute '__dict__' of 'Person' objects>}
 
+总结：
+
+引用静态变量
+  1.类名.__dict__['静态变量名'] 可以查看，但是不能删改
+  2.类名.静态变量名 直接就可以访问,可以删改
+  删除一个静态变量 del 类名.静态变量名
+
 2.
 
 
