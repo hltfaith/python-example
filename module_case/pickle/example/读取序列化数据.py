853799bbd652386e40e14192170b23432439a3e0
Changhao <wu_chang_hao@qq.com>
2019-05-29 23:19:03 +0800

    Create 读取序列化数据.py

---
+++ module_case/pickle/example/读取序列化数据.py
@@ -0,0 +1,13 @@
+import pprint, pickle
+#使用pickle模块从文件中重构python对象
+
+pkl_file = open('data.pkl', 'rb')
+
+data1 = pickle.load(pkl_file)
+pprint.pprint(data1)
+
+data2 = pickle.load(pkl_file)
+pprint.pprint(data2)
+
+pkl_file.close()
+
